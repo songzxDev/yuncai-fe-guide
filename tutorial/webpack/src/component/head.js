facf896cd3ecaf8e6101295219adbc7cd14ffbed
windknow <songhlc@yonyou.com>
2018-07-05 20:02:52 +0800

    添加webpack打包后原理分析

---
+++ tutorial/webpack/src/component/head.js
@@ -0,0 +1,9 @@
+function isHead (task) {
+  if (task > 0)
+    return true
+  else
+    return false
+}
+module.exports = {
+  isHead: isHead
+}
---
+++ tutorial/webpack/src/index.js
@@ -0,0 +1,6 @@
+import {isHead} from './component/head'
+import {add, minus} from './util'
+
+console.log(add(4, 5))
+console.log(minus(5, 1))
+console.log(isHead(6))
